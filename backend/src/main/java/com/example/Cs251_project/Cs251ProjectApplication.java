d2fe660614c18ec06396b2ff6ac64e34b6fa318e
newbie <thanagon.pay@dome.tu.ac.th>
2025-02-26 21:43:07 +0700

    Springboot-file-structure

---
+++ backend/src/main/java/com/example/Cs251_project/Cs251ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cs251_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs251ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs251ProjectApplication.class, args);
+	}
+
+}
