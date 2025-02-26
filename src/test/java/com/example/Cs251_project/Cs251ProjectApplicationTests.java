d8a7ae5107eadea60804e8e377df4fc6f4ae3356
newbie <thanagon.pay@dome.tu.ac.th>
2025-02-26 16:22:36 +0700

    Springboot-install

---
+++ src/test/java/com/example/Cs251_project/Cs251ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cs251_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs251ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
