0dda2e05a67f6295a913b182e958cc4dec61646f
imnotsunday <nuttinee.bua@dome.tu.ac.th>
2025-05-10 19:47:52 +0700

    Login&Register pages DONE !

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LoginPage from '../page/LoginPage.vue';
-import SignupPage from '../page/SignupPage.vue';
-import HomePage from '../page/HomePage.vue';
+import LoginPage from '../../frontend/src/page/LoginPage.vue';
+import SignupPage from '../../frontend/src/page/SignupPage.vue';
+import HomePage from '../../frontend/src/page/HomePage.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginPage },
   { path: '/signup', component: SignupPage },
-  { path: '/home', component: HomePage },
+  { path: '/home', name: 'homepage', component: HomePage  },
 ];
 
 export default createRouter({
