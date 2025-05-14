b8a35a354a8a38471081f2cbfd724dd01f575c9e
PalmSiri <sirikorn.tir@gmail.com>
2025-05-15 00:42:55 +0700

    Update Noti

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cs251-project
+
+## Back-end : Java with springbootframework , MySQL 
+## Front–end : Vue.js , HTML , CSS , Javascript 
+## Environment : Docker , Docker-compose 
+## Version Control : Github 
+## Design : Figma 
+## Project Management : Trello , Discord 
---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      showPassword: false
+    };
+  },
+  methods: {
+    handleLogin() {
+      // Simulate a successful login and navigate to the homepage
+      alert(`Logging in as: ${this.username}`);
+      this.$router.push({ name: '../public/home.html' }); // Replace 'homepage' with your actual route name
+    },
+    togglePassword() {
+      this.showPassword = !this.showPassword;
+    }
+  }
+};
+</script>
